9a45b7d9beecb4fd3fb55789c488aa213f9ed2d4
NotScottt <scott2005.sz@gmail.com>
2023-07-12 10:45:33 +0200

    New

--- src/WeatherCrawler.py
+++ src/WeatherCrawler.py
@@ -1,21 +1,10 @@
 ## Imports
-from infi.systray import SysTrayIcon 
-import requests
-from bs4 import BeautifulSoup
-from tkinter import messagebox
-import os
+from infi.systray import SysTrayIcon
 import functions
 
-## Static Link
-r = requests.get("https://forecast7.com/de/50d9811d03/erfurt/")
-doc = BeautifulSoup(r.content, 'html.parser')
+menu_options = (("Wetter Heute", None, functions.show_weather), ("Wetter 7 Tage", None, functions.return_x_days))
 
-
-menu_options = (("Wetter Heute", None, functions.show_weather(doc)),)
-# menu_options = (("Wetter Heute", None, None), ("Wetter 7 Tage", None, None))
-
-
-if functions.check_weather_ico(functions.strip_item(functions.get_temp(1,))) == True:
+if functions.check_weather_ico(functions.strip_item(functions.get_temp(1))) == True:
     systray = SysTrayIcon("sun.ico", "Scott's Wetter", menu_options)
 else:
     systray = SysTrayIcon("rain.ico", "Scott's Wetter", menu_options)
